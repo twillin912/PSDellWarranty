9be4fa9ac60176344610f0fa5da9dc65424dd7e1
Trent Willingham <TrentWillingham@gmail.com>
2016-11-07 11:43:31 -0500

    AppVeyor configuration

---
+++ build/build.ps1
@@ -0,0 +1,29 @@
+<#
+#>
+[CmdletBinding()]
+
+Param
+(
+    [string] $Task = 'Build'
+)
+
+$RequiredModules = @('BuildHelpers','Psake','Pester','PlatyPS','PSScriptAnalyzer')
+
+Write-Verbose -Message "Set package provider and verify required packages are installed."
+    Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
+
+foreach ( $Module in $RequiredModules )
+{
+    if ( !( Get-Module -ListAvailable -Name $Module ) )
+    {
+        Install-Module -Name $Module -Scope CurrentUser -Force | Out-Null
+    }
+}
+
+Write-Verbose -Message "Set build environment using the BuildHelpers module."
+    Set-BuildEnvironment
+
+Write-Verbose -Message 'Invoking psake build script.'
+    Invoke-PSake -buildFile $env:BHProjectPath\Build\build.psake.ps1 -taskList $Task -nologo
+
+exit ( [int]( -not $psake.build_success ) )
